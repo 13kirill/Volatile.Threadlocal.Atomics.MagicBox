bd03aa0ebf4e1fc34ec36b2079fa2f1d12e56a26
Kirill Zubkov <kirill131313@gmail.com>
2022-05-04 12:14:55 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,30 @@ import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    //Переменная "а" символизирует тумблер и счётчик открытия коробки.
-    //Когда "а" увеличивается, т.е. пользователь открывает коробку,
+    //Переменная "openingCounter" символизирует тумблер и счётчик открытия коробки.
+    //Когда "openingCounter" увеличивается, т.е. пользователь открывает коробку,
     //переменная "closingCounter" сравнивает себя с переменной "а" и
-    //если переменная "а" увеличилась (коробка открылась), то
+    //если переменная "openingCounter" увеличилась (коробка открылась), то
     //переменная "closingCounter" тоже увеличивается (коробка закрылась)
 
-    volatile public static int a = 0;
+    volatile public static int openingCounter = 0;
     public static final int BOX_CLOSURE = 1000;
+    private static UserThread userThread = new UserThread();
+    private static BoxThread boxThread = new BoxThread();
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        new UserThread().start();
-        new BoxThread().start();
+
+        userThread.start();
+        boxThread.start();
+
     }
 
     static class UserThread extends Thread {
         @Override
         public void run() {
-            while (a < 5) {
+            while (openingCounter < 5) {
                 {
-                    System.out.printf("Пользователь открывает коробку коробку %s раз \n", ++a);
+                    System.out.printf("Пользователь открывает коробку коробку %s раз \n", ++openingCounter);
                     try {
                         Thread.sleep(BOX_CLOSURE);
                     } catch (InterruptedException e) {
@@ -29,17 +33,22 @@ public class Main {
                     }
                 }
             }
+            boxThread.interrupt();
         }
     }
 
     static class BoxThread extends Thread {
         int closingCounter = 0;
+
         @Override
         public void run() {
-            while (closingCounter < 5) {
-                if (closingCounter != a) {
-                    System.out.printf("Коробка закрывается %s раз \n", a);
-                    closingCounter = a;
+            while (true) {
+                if (closingCounter != openingCounter) {
+                    System.out.printf("Коробка закрывается %s раз \n", openingCounter);
+                    closingCounter = openingCounter;
+                }
+                if (isInterrupted()) {
+                    return;
                 }
             }
         }
